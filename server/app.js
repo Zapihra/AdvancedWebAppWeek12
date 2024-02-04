ef437c945561215893bd09b9043d175bdf0cb49b
Zapihra <iida.vaaja@gmail.com>
2024-02-04 23:11:32 +0200

    client and server basics

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const mongoDB = "mongodb://127.0.0.1:27017/school"
+mongoose.connect(mongoDB);
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"))
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ server/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose')
+
+const booksSchema = new mongoose.Schema({
+  name: {type: String},
+  author: {type: String},
+  pages: {type: Number}
+});
+
+const Book = mongoose.model('Books', booksSchema)
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/api/book', function(req,res) {
+  var body = req.body
+
+  const book = new Book({
+    name: body.name,
+    author: body.author,
+    pages: body.pages
+  })
+  book.save()
+
+  res.send("ok")
+})
+
+
+module.exports = router;
